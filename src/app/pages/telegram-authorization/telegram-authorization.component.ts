f7208c737b2c2f323ff7bb386c03fac0a5d874f7
xdufo <zxcvbnm0951668159@gmail.com>
2024-11-30 16:39:10 +0200

    test commit

--- src/app/pages/telegram-authorization/telegram-authorization.component.ts
+++ src/app/pages/telegram-authorization/telegram-authorization.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 import { AuthService } from '../../services/AuthService';
 
 @Component({
   selector: 'app-telegram-authorization',
   standalone: true,
-  imports: [],
   templateUrl: './telegram-authorization.component.html',
   styleUrls: ['./telegram-authorization.component.scss'],
 })
-export class TelegramAuthorizationComponent {
-  constructor(private authService: AuthService) {}
+export class TelegramAuthorizationComponent implements AfterViewInit {
+  constructor(
+    private authService: AuthService,
+    private elementRef: ElementRef
+  ) {}
+
+  // Метод, вызываемый после инициализации представления
+  ngAfterViewInit(): void {
+    // Создаем скрипт
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://telegram.org/js/telegram-widget.js?22';
+    script.setAttribute('data-telegram-login', 'beerUAbot'); // Замените 'beerUAbot' на ваше имя бота
+    script.setAttribute('data-size', 'large');
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    script.setAttribute('data-request-access', 'write');
+
+    // Добавляем скрипт в контейнер
+    const container = this.elementRef.nativeElement.querySelector(
+      '#telegram-widget-container'
+    );
+    container.appendChild(script);
+  }
 
   onTelegramAuth(user: any) {
     console.log('Авторизован пользователь:', user);
